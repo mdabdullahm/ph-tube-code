58dbb865252d2a0ca2dead16b75447cc4b63cdb5
mdabdullahm <you@example.com>
2024-11-20 20:20:27 +0600

    addad video.js

--- script/video.js
+++ script/video.js
@@ -1,6 +1,17 @@
 console.log('video script added');
 
+// create loadCategories
 const loadCategories = () => {
-    console.log("loadCategories created");
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => DisplayCategories(data.categories))
+    .Catch((error) => console.log(error));
 };
+// create DisplayCategories
+const DisplayCategories = (data) => {
+    //  add Data in html
+    console.log(data);
+};
+
 loadCategories();
