9aa3963802f7bacd0b802b9ff821d461dfcd0870
mdabdullahm <you@example.com>
2024-11-21 19:01:39 +0600

    addad indx.html and video.js

--- script/video.js
+++ script/video.js
@@ -9,9 +9,16 @@ const loadCategories = () => {
     .Catch((error) => console.log(error));
 };
 // create DisplayCategories
-const DisplayCategories = (data) => {
-    //  add Data in html
-    console.log(data);
+const DisplayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories")
+    categories.forEach((item) =>{
+        console.log(item);
+
+        const button = document.createElement("button");
+        button.classList = "btn";
+        button.innerText = item.category;
+        categoryContainer.append(button);
+    });
 };
 
 loadCategories();
